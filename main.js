ebc5bf2b3f13ae87b520d3e96cd19576e2b13285
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-09-20 11:46:13 +0200

    panel

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var courses = Array.prototype.slice.call(document.getElementsByClassName("parcours"))
+var courses = Array.prototype.slice.call(document.getElementsByClassName("categorie"))
 
 console.log(courses)
 
@@ -7,12 +7,18 @@ function testFunction(elt) {
         console.log("ok")
         console.log(elt.children[2].classList)
         var classes = [...elt.children[2].classList]
+        var panel = elt.parentNode.children[1];
+        console.log(panel)
         if (classes.includes('arrow--opened')) {
             elt.children[2].classList.add("arrow--closed")
             elt.children[2].classList.remove("arrow--opened")
+            panel.classList.add("panel--closed")
+            panel.classList.remove("panel--opened")
         } else {
             elt.children[2].classList.remove("arrow--closed")
             elt.children[2].classList.add("arrow--opened")
+            panel.classList.remove("panel--closed")
+            panel.classList.add("panel--opened")
         }
     };
 
@@ -22,3 +28,4 @@ courses.forEach(element => {
     element.addEventListener("click", testFunction(element), false);
     console.log(element)
 });
+
